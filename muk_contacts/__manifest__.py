920885cf2ffc9c85ce4a520ea2178dd040f9bc7b
Dick Knol <dgknol78@gmail.com>
2026-01-08 14:27:32 +0100

    Cleanup contact_number

--- muk_contacts/__manifest__.py
+++ muk_contacts/__manifest__.py
@@ -1,5 +1,5 @@
 {
-    'name': 'MuK Contacts', 
+    'name': 'MuK Contacts',
     'summary': 'Improves the contact app',
     'description': '''
         This module improves and extends the contact app
@@ -7,7 +7,7 @@
     ''',
     'version': '19.0.1.0.3',
     'category': 'Sales/CRM',
-    'license': 'LGPL-3', 
+    'license': 'LGPL-3',
     'author': 'MuK IT',
     'website': 'http://www.mukit.at',
     'live_test_url': 'https://youtu.be/UE_owy582jY',
@@ -21,11 +21,8 @@
         'muk_web_utils',
     ],
     'data': [
-        'data/ir_sequence.xml',
         'templates/ir_qweb_widget.xml',
         'views/res_partner.xml',
-        'views/res_config_settings.xml',
-        'views/base_partner_merge.xml',
     ],
     'images': [
         'static/description/banner.png',
--- muk_contacts/models/__init__.py
+++ muk_contacts/models/__init__.py
@@ -1,2 +1,2 @@
 from . import res_partner
-from . import res_config_settings
+rom . import res_config_settings

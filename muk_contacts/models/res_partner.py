040b23fdd5125f7a556d569c48ac4f41d0bb7497
Dick Knol <dgknol78@gmail.com>
2026-01-08 14:06:49 +0100

    Remove contact_number

--- muk_contacts/models/res_partner.py
+++ muk_contacts/models/res_partner.py
@@ -5,30 +5,30 @@ from odoo.exceptions import UserError
 class Partner(models.Model):
 
     _inherit = 'res.partner'
-    _rec_names_search = [
-        'complete_name',
-        'email',
-        'ref',
-        'vat',
-        'company_registry',
-        'contact_number'
-    ]
-    
+    # _rec_names_search = [
+    #     'complete_name',
+    #     'email',
+    #     'ref',
+    #     'vat',
+    #     'company_registry',
+    #     'contact_number'
+    # ]
+
     # ----------------------------------------------------------
     # Fields
     # ----------------------------------------------------------
-    
-    contact_number = fields.Char(
-        string="Contact Number",
-        tracking=True,
-        copy=False,
-        index=True,
-    )
+
+    # contact_number = fields.Char(
+    #     string="Contact Number",
+    #     tracking=True,
+    #     copy=False,
+    #     index=True,
+    # )
 
     linked_user_id = fields.Many2one(
-        compute='_compute_linked_user_id', 
-        search='_search_linked_user_id', 
-        comodel_name='res.users', 
+        compute='_compute_linked_user_id',
+        search='_search_linked_user_id',
+        comodel_name='res.users',
         string="Linked User",
         readonly=True,
         store=False
@@ -56,32 +56,32 @@ class Partner(models.Model):
     # ----------------------------------------------------------
     # Index
     # ----------------------------------------------------------
-    
-    _unique_contact_number = models.UniqueIndex(
-        "(contact_number) WHERE contact_number IS NOT NULL AND parent_id IS NULL",
-        "Another entry with the same contact number already exists.",
-    )
-        
+
+    # _unique_contact_number = models.UniqueIndex(
+    #     "(contact_number) WHERE contact_number IS NOT NULL AND parent_id IS NULL",
+    #     "Another entry with the same contact number already exists.",
+    # )
+
     #----------------------------------------------------------
     # Helper
     #----------------------------------------------------------
-    
-    @api.model
-    def _commercial_fields(self):
-        return super()._commercial_fields() + [
-            'contact_number'
-        ]
 
-    @api.model
-    def _get_next_contact_number(self, raise_exception=False):
-        contact_number = self.env['ir.sequence'].next_by_code(
-            'contact.number'
-        )
-        if not contact_number and raise_exception:
-            raise UserError(_(
-                "The contact number sequence couldn't be found."
-            ))
-        return contact_number
+    # @api.model
+    # def _commercial_fields(self):
+    #     return super()._commercial_fields() + [
+    #         'contact_number'
+    #     ]
+
+    # @api.model
+    # def _get_next_contact_number(self, raise_exception=False):
+    #     contact_number = self.env['ir.sequence'].next_by_code(
+    #         'contact.number'
+    #     )
+    #     if not contact_number and raise_exception:
+    #         raise UserError(_(
+    #             "The contact number sequence couldn't be found."
+    #         ))
+    #     return contact_number
 
     # ----------------------------------------------------------
     # Functions
@@ -99,7 +99,7 @@ class Partner(models.Model):
     #----------------------------------------------------------
     # Actions
     #----------------------------------------------------------
-    
+
     def action_view_partner(self):
         self.ensure_one()
         return {
@@ -111,30 +111,30 @@ class Partner(models.Model):
             'views': [(self.env.ref('base.view_partner_form').id, 'form')],
             'context': self.env.context
         }
-        
-    def action_generate_contact_number(self):
-        self.ensure_one()
-        self.write({
-            'contact_number': self._get_next_contact_number(
-                raise_exception=True
-            )
-        })
-    
+
+    # def action_generate_contact_number(self):
+    #     self.ensure_one()
+    #     self.write({
+    #         'contact_number': self._get_next_contact_number(
+    #             raise_exception=True
+    #         )
+    #     })
+
     #----------------------------------------------------------
     # Compute
     #----------------------------------------------------------
 
-    @api.depends('contact_number')
-    @api.depends_context('show_contact_number')
-    def _compute_display_name(self):
-        super()._compute_display_name()
-        if self.env.context.get('show_contact_number'):
-            for record in self.filtered('contact_number'):
-                record.display_name = (
-                    f"--[{record.contact_number}]-- {record.display_name}"
-                    if self.env.context.get('formatted_display_name')
-                    else f"[{record.contact_number}] {record.display_name}"
-                )
+    # @api.depends('contact_number')
+    # @api.depends_context('show_contact_number')
+    # def _compute_display_name(self):
+    #     super()._compute_display_name()
+    #     if self.env.context.get('show_contact_number'):
+    #         for record in self.filtered('contact_number'):
+    #             record.display_name = (
+    #                 f"--[{record.contact_number}]-- {record.display_name}"
+    #                 if self.env.context.get('formatted_display_name')
+    #                 else f"[{record.contact_number}] {record.display_name}"
+    #             )
 
     def _search_linked_user_id(self, operator, value):
         return [('user_ids', operator, value)]
@@ -160,12 +160,12 @@ class Partner(models.Model):
     # ORM
     #----------------------------------------------------------
 
-    @api.model_create_multi
-    def create(self, vals_list):
-        for vals in vals_list:
-            if (
-                not vals.get('contact_number', False) and 
-                not vals.get('parent_id', False)
-            ):
-                vals['contact_number'] = self._get_next_contact_number()
-        return super().create(vals_list)
+    # @api.model_create_multi
+    # def create(self, vals_list):
+    #     for vals in vals_list:
+    #         if (
+    #             not vals.get('contact_number', False) and
+    #             not vals.get('parent_id', False)
+    #         ):
+    #             vals['contact_number'] = self._get_next_contact_number()
+    #     return super().create(vals_list)
